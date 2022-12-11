1555cda3f0a46e1b93c7d6b51ba3e6e19f0de501
Joshua Gunter <joshua.d.gunter@gmail.com>
2022-12-11 20:24:12 +0000

    Add solution for day 11 part 2.

--- 11/aoc_2022_11.py
+++ 11/aoc_2022_11.py
@@ -1,6 +1,7 @@
 """Advent of Code day 11."""
 
 from dataclasses import dataclass
+from functools import reduce
 
 
 def get_input(name):
@@ -38,15 +39,43 @@ def parse_monkeys(problem_input):
     return [parse_monkey(chunk) for chunk in chunks]
 
 
+def _gcd(a, b):
+    """Compute the greatest common divisor of a pair of integers.
+    Taken from https://en.wikipedia.org/wiki/Euclidean_algorithm#Implementations."""
+    while b != 0:
+        temp = b
+        b = a % b
+        a = temp
+    return a
+
+
+def gcd(*args):
+    """Compute the greatest common divisor of a list of integers."""
+    assert len(args) >= 2
+    result = _gcd(args[0], args[1])
+    for arg in args[3:]:
+        result = _gcd(result, arg)
+    return result
+
+
+def lcm(*args):
+    """Find the lowest common multiple of a list of integers."""
+    return int(reduce(lambda x, y: x*y, args)/gcd(*args))
+
+
 def evaluate_round(monkeys, divide_worry_levels=True):
-    """Evaluate a round of monkeys throwing objects. 
+    """Evaluate a round of monkeys throwing objects.
     Returns a new list of monkeys with updated state."""
+    # Perform all worry level computations modulo the lcm(monkey test divisors) in
+    # order to keep worry level sizes relatively small.
+    N = lcm(*(monkey.test for monkey in monkeys))  # pylint: disable=invalid-name
     for monkey in monkeys:
         for worry_level in monkey.items:
             monkey.num_inspections += 1
             worry_level = monkey.operation(worry_level)
             if divide_worry_levels:
                 worry_level //= 3
+            worry_level = worry_level % N
             if worry_level % monkey.test == 0:
                 monkeys[monkey.on_true].items.append(worry_level)
             else:
@@ -67,6 +96,10 @@ def test():
     for _ in range(20):
         monkeys = evaluate_round(monkeys)
     assert monkey_business(monkeys) == 10605, monkeys
+    monkeys = parse_monkeys(get_input("test_input.txt"))
+    for _ in range(10000):
+        monkeys = evaluate_round(monkeys, divide_worry_levels=False)
+    assert monkey_business(monkeys) == 2713310158, monkeys
     print("Tests passed!")
 
 
